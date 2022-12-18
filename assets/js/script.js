d8cd6ca73c3869700b7f4de570e014065551e245
Lferg98 <lachieferg98@hotmail.com>
2022-12-18 12:48:17 +1100

    fixed quiz integration, added more styling, final commit

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,15 +7,15 @@ var codeQuiz = [
         b: '<javascript>',
         c: '<script>',
         d: '<scripting>',
-        correctAns: 'b'
+        answer: 'c'
     }, 
     {
-        question: 'Which of the following is not a way to wrtie a function in JavaScript?',
+        question: 'Which of the following is not a way to write a function in JavaScript?',
         a: 'let myFunc = function() { ... };',
         b: 'const myFunc = () => { ... };',
         c: 'var myFunc() = function { ... };',
         d: 'function myFunc() { ... };',
-        correctAns: 'c'
+        answer: 'c'
     },
     {
         question: 'What is the correct JavaScript syntax to change the content of the HTML element below?',
@@ -23,7 +23,7 @@ var codeQuiz = [
         b: 'document.getElement("p").innerHTML = "Hello World!";',
         c: 'document.getElementById("demo").innerHTML = "Hello World!";  ',
         d: 'document.getElementByName("p").innerHTML = "Hello World!";',
-        correctAns: 'c'
+        answer: 'c'
     },
     {
         question: 'How to write an IF statement for executing some code if "i" is NOT equal to 5?',
@@ -31,7 +31,7 @@ var codeQuiz = [
         b: 'if (i != 5) ',
         c: 'if (i <> 5)',
         d: 'if i =! 5 then',
-        correctAns: 'b'
+        answer: 'b'
     },
     {
         question: 'What type of data is "56"?',
@@ -39,96 +39,52 @@ var codeQuiz = [
         b: 'number',
         c: 'boolean',
         d: 'string',
-        correctAns: 'd'
+        answer: 'd'
     }
 ]
 
+
 var background = document.querySelector("body");
-var cardEl = document.querySelector("#card");
 var startBtn = document.querySelector("#start-btn");
-var scoreEL = document.querySelector(".score");
 var quizEl = document.querySelector(".quiz-container");
 var endEl = document.querySelector(".end");
+var scoreEl = document.querySelector(".score");
 var questionCounter = 0;
 var currentScore = 99;
 var highScores = [];
 
+// starts score counter upon quiz start
+var scoreCounter = function() {
+    scoreEl.textContent = "Current score: 100"
 
-// start quiz function
-//QUIZ STRUCTURE 
- //QUIZ QUESTIONS SECTION 
- //QUIZ ANSWER SELECTIONS SECTION WITH BUTTONS FOR EACH ANSWER SELECTION
- //QUIZ CORRECT/INCORRECT SECTION
-var quickQuiz = function() {
-
-    document.querySelector("#instructions").remove();
-
-    // creates container div for questions
-    var questionEl = document.createElement("div");
-    questionEl.className = "question";
-    cardEl.appendChild(questionEl);
-
-    // creates container div for answers
-    var answersEl = document.createElement("div");
-    answersEl.className = "answer-grid";
-    cardEl.appendChild(answersEl);
-
-    // button A
-    var choiceA = document.createElement("div");
-    choiceA.className = "answer-grid-item";
-    answersEl.appendChild(choiceA);
-
-    var btnA = document.createElement("button");
-    btnA.className = "btn";
-    btnA.id = "btn-a";
-    choiceA.appendChild(btnA);
-
-    // button B
-    var choiceB = document.createElement("div");
-    choiceB.className = "answer-grid-item";
-    answersEl.appendChild(choiceB);
-
-    var btnB = document.createElement("button");
-    btnB.className = "btn";
-    btnB.id = "btn-b";
-    choiceB.appendChild(btnB);
-
-    // button C
-    var choiceC = document.createElement("div");
-    choiceC.className = "answer-grid-item";
-    answersEl.appendChild(choiceC);
-
-    var btnC = document.createElement("button");
-    btnC.className = "btn";
-    btnC.id = "btn-c";
-    choiceC.appendChild(btnC);
-
-    // button D
-    var choiceD = document.createElement("div");
-    choiceD.className = "answer-grid-item";
-    answersEl.appendChild(choiceD);
-
-    var btnD = document.createElement("button");
-    btnD.className = "btn";
-    btnD.id = "btn-d";
-    choiceD.appendChild(btnD);
+    var scoreInterval = setInterval(function() {
+        if (currentScore > 0 && questionCounter < codeQuiz.length) {
+            scoreEl.textContent = "Current score: " + currentScore;
+            currentScore--
+        }
+        else {
+            clearInterval(scoreInterval);
+            endQuiz();
+        }
+    }, 1000);
 }
 
+var createQuiz = function() {
+    document.querySelector("#instructions").remove();
+    quizEl.classList.remove("hide")
 
+    nextQues(questionCounter);
+    scoreCounter();
+}
 
-startBtn.addEventListener("click", createQuiz)
-
-
-
-// add button event listeners/iteration for each question
-
-var nextQuestion = function(index) {
+// iterates through questions and answers
+var nextQues = function(index) {
     var questionHeader = document.querySelector(".question-header");
     var questionEl = document.querySelector(".question");
-    var btnA = document.querySelector("#btn-a");
-    var btnB = document.querySelector("#btn-b");
-    var btnC = document.querySelector("#btn-c");
-    var btnD = document.querySelector("#btn-d");
+    var btnA = document.getElementById("btn-a");
+    var btnB = document.getElementById("btn-b");
+    var btnC = document.getElementById("btn-c");
+    var btnD = document.getElementById("btn-d");
 
     questionHeader.textContent = "Question #" + parseInt(index + 1)
     questionEl.textContent = codeQuiz[index].question;
@@ -173,46 +129,10 @@ var checkAnswer = function(event) {
     }
 }
 
-
-
-
-
-
-
-
-
-//ON CLICK OF START BUTTON, TIMER STARTS AND FIRST QUESTION IS DISPLAYED
-
-var scoreCounter = function() {
-    scoreEl.textContent = "Current Score: 100";
-
-    var scoreInterval = setInterval(function() {
-        if (currentScore > 0 && questionCounter < quickQuiz.length) {
-            scoreEl.textContent = "Current Score: " + currentScore;
-            currentScore--;
-        }
-        else {
-            clearInterval(scoreInterval);
-            // add end quiz function 
-        }
-    }, 1000);
-}
-
-
-
-// add startquiz & endquiz functions
-
-var createQuiz = function() {
-    document.querySelector("#instructions").remove();
-    quizEl.classList.remove("hide");
-
-}
-
-
 var endQuiz = function() {
     quizEl.remove();
     scoreEl.remove();
-    
+ 
     endEl.innerHTML = "<h2 class='end-title'>It's all over ! Well Done !</h2><p>Your final score is " + currentScore + ".  Please enter your name.</p>";
 
     var scoreForm = document.createElement("form");
@@ -235,18 +155,37 @@ var endQuiz = function() {
     nameBtn.addEventListener("click", saveScore);
 }
 
+var saveScore = function() {
+    event.preventDefault()
 
+    var playerName = document.querySelector("input[name='player-name']").value;
 
+    if (!playerName) {
+        alert("Please enter your name!")
+    }
+    else {
+        var scoreObj = {
+            name: playerName,
+            score: currentScore
+        }
+    
+        highScores.push(scoreObj);
+        document.querySelector("#score-form").reset();
+        localStorage.setItem("scores", JSON.stringify(highScores));
+        document.location.href = "highscore.html";
+    }
+}
 
+var loadScores = function() { 
+    highScores = localStorage.getItem("scores");
 
-//ADD TIMER FUNCTION 
-//DEDUCT POINTS FOR INCORRECT ANSWERS
+    if (!highScores) {
+        highScores = [];
+        return false;
+    }
 
-//ADD LOCAL STORAGE FUNCTION TO SAVE HIGH SCORES
-//ADD OPTION TO TO ENTER NAME AND SAVE HIGH SCORES
+    highScores = JSON.parse(highScores);
+}
 
-//CREATE A HIGH SCORE LIST PAGE
-//ADD OPTION TO CLEAR HIGH SCORES
-
- //FINE TUNE STYLE ELEMENTS AND ADDITIONAL FEATURES
-//ADD HOVER EFFECTS TO BUTTONS
+loadScores();
+startBtn.addEventListener("click", createQuiz)
